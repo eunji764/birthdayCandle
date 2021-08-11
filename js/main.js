f42133ebb44c61e25e4096cbd152a2fd9ba9e7b0
eunji764 <rladmswl0216@naver.com>
2021-08-12 00:04:46 +0900

    enter keypress 동작 추가

--- js/main.js
+++ js/main.js
@@ -19,4 +19,22 @@ btn.addEventListener('click',function(){
   }
 })
 
-input.addEventListener('onkeypress', )
+input.addEventListener('keypress',function(e){
+  if (e.keyCode === 13){
+    {
+      const birthYear = input.value
+      if(!isNaN(birthYear) && birthYear.toString().length === 4){
+        
+        americanAge = thisYear - birthYear
+        koreanAge = americanAge + 1
+        
+        result.innerText = koreanAge
+        resultDeco.style.display = 'block'
+        
+      } else {
+        alert('4자리 숫자로 입력해주십시오.')
+      }
+    }
+
+  }
+} )
